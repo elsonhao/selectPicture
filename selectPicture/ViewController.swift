2ea4561dad87c63e786617e54980f80f4196a103
Elson <huangyuhao@huangyuhaodeMacBook-Pro.local>
2017-03-17 19:10:50 +0800

    按下圖片能選取拍照或從圖庫取用照片

--- selectPicture/ViewController.swift
+++ selectPicture/ViewController.swift
@@ -8,11 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var myImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+       myImage.isUserInteractionEnabled = true
+    myImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(showPickerMenu)))
+    }
+    
+    func showPickerMenu(){
+        let alertcontroller = UIAlertController(title: "選擇哪種模式", message: "可拍照或從圖庫取用", preferredStyle: .actionSheet)
+        let alertaction = UIAlertAction(title: "圖庫取用", style: .default) { (action) in
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        let alertaction2 = UIAlertAction(title: "拍照", style: .default) { (action) in
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        alertcontroller.addAction(alertaction)
+        alertcontroller.addAction(alertaction2)
+        present(alertcontroller, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        myImage.image = image
+        self.dismiss(animated: true, completion: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
